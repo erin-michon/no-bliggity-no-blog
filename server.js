a71b67088688ff536fcf296992f8a5254ab9c421
Erin M <erin.michon@gmail.com>
2022-01-19 18:59:35 -0500

    updated secret

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 //Review session secret - store in dotenv
 const sess = {
-  secret: process.env.DB_SECRET,
+  secret: 'Super duper secret',
   cookie: {
     // Session will automatically expire in 10 minutes
     expires: 10 * 60 * 1000
